aaf0349150d1369d0a74adebabf8da74d7ca668e
TDanusorn <danusorn.tide@gmail.com>
2018-12-09 10:57:36 +0700

    dad

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('cost/', views.post_detail, name='post_detail'),
     path('care/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
